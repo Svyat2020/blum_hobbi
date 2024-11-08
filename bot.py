8498f6ae66129c391e0e0eb5a858f389338ee995
Svyat2020 <69816145+Svyat2020@users.noreply.github.com>
2024-11-08 19:28:40 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,19 +4,20 @@ from aiogram.utils import executor
 
 # Замените 'YOUR_BOT_TOKEN' на токен вашего бота, который вы получили от BotFather
 API_TOKEN = '7437889063:AAGakOxzoGBnBON2gSYfzseRUurV5MTcfP0'
-WEB_APP_URL = 'https://your-domain.com/index.html'  # Ссылка на вашу игру (URL после публикации)
+WEB_APP_URL = 'https://Svyat2020.github.io/blum_hobbi/index.html'  # Ссылка на вашу игру (URL после публикации)
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=7437889063:AAGakOxzoGBnBON2gSYfzseRUurV5MTcfP0)
 dp = Dispatcher(bot)
 
 # Команда /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Нажми на кнопку ниже, чтобы начать зарабатывать Blum монеты!", reply_markup=types.InlineKeyboardMarkup().add(
-        types.InlineKeyboardButton("Играть", web_app=types.WebAppInfo(url=WEB_APP_URL))
-    ))
+        types.InlineKeyboardButton("Играть", web_app=types.WebAppInfo(url=https://Svyat2020.github.io/blum_hobbi/index.html))
+    )
+  )
 
-if name == 'main':
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
